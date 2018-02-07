d80fe5abaf597dca18c6b15f06ff522ca82d1304
Unity Technologies <@unity.com>
2018-02-08 00:00:00 +0100

    com.unity.scriptablebuildpipeline@0.0.5-preview # Changelog All notable changes to this package will be documented in this file.
    The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
    and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
    
    ## [0.0.5-preview] - 2018-02-08
    - Initial submission for package distribution

---
+++ Editor/Utilities/BuildStateCleanup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace UnityEditor.Build.Utilities
+{
+    public class BuildStateCleanup : IDisposable
+    {
+        string m_TempPath;
+
+        public BuildStateCleanup(string tempBuildPath)
+        {
+            m_TempPath = tempBuildPath;
+            Directory.CreateDirectory(m_TempPath);
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(m_TempPath))
+                Directory.Delete(m_TempPath, true);
+        }
+    }
+}
