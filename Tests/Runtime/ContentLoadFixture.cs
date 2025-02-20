169c6003d9d9d916dc7a00b358be914f50a12138
Unity Technologies <@unity>
2025-02-20 00:00:00 +0000

    com.unity.scriptablebuildpipeline@2.3.5 ## [2.3.5] - 2025-02-20 - Fixed serialization tests

--- Tests/Runtime/ContentLoadFixture.cs
+++ Tests/Runtime/ContentLoadFixture.cs
@@ -206,6 +206,10 @@ namespace UnityEditor.Build.Pipeline.Tests.ContentLoad
         public void Setup()
         {
 #if UNITY_EDITOR
+            //return here if the platform isn't installed. There appears to be an issue where the Setup runs for a test suite, even if all the tests would otherwise be ignored
+            if (!string.IsNullOrEmpty(ContentPipeline.CanBuildPlayer(EditorUserBuildSettings.activeBuildTarget, EditorUserBuildSettings.selectedBuildTargetGroup, "tempFolder")))
+                return;
+
             DefaultBuild();
 #endif
         }
